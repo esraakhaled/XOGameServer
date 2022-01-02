bfea9fefb422009a997e617366317098c19e2de3
Ahmed Mohamed Abdallah <benten12536.com>
2022-01-02 21:21:36 +0200

    make check for user as login and register and finish register and log on

--- src/xogameserver/Client.java
+++ src/xogameserver/Client.java
@@ -28,15 +28,14 @@ import serialize.models.Register;
  */
 public class Client extends Thread {
 
-    //DataInputStream dis;
-    //PrintStream ps;
     OutputStream os;
     InputStream is;
     Socket cs;
     ObjectOutputStream objectOutputStream;
     DataAccessLayer dataAccessLayer;
     public static Map<String, Client> clientsVector = new HashMap<String, Client>();
-    public static Map<String, Client> attempsUser = new HashMap<String, Client>();
+    // public static Map<String, Client> attempsUser = new HashMap<String, Client>();
+    Player playerDB = null;
 
     public Client(Socket _cs) {
         try {
@@ -74,9 +73,11 @@ public class Client extends Thread {
                 Object obj = objectInputStream.readObject();
                 if (obj instanceof Login) {
                     Login login = (Login) obj;
+                    clientsVector.put(login.getUserName(), this);
                     sendLoginMessage(login);
                 } else if (obj instanceof Register) {
                     Register register = (Register) obj;
+                    clientsVector.put(register.getUserName(), this);
                     sendRegisterMessage(register);
                 } else if (obj instanceof Connection) {
                     Connection connection = (Connection) obj;
@@ -92,49 +93,82 @@ public class Client extends Thread {
     }
 
     void sendLoginMessage(Login login) {
-        Player playerDB = null;
-
+        boolean status = false;
+        playerDB = null;
         try {
 //change
             Player playerLogin = new Player(login.getUserName(), login.getPassword());
-            boolean status;
+
 // check if it exist in DB
-            if (dataAccessLayer.checkPlayer(playerLogin)) {
+            if (dataAccessLayer.checkPlayerForLogin(playerLogin)) {
                 status = true;
                 playerDB = dataAccessLayer.getPlayer(playerLogin.getUserName());
+                dataAccessLayer.updatePlayerStatusAvailable(playerDB.getUserName());
+                dataAccessLayer.updatePlayerStatusOnline(playerDB.getUserName());
             } else {
                 status = false;
             }
 // get Player
 
-            /*
-Client.clientsVector.entrySet().stream()
-.filter(map -> (map.getKey()).equals(login.getUserName()))
-.forEach(map -> {  });
-             */
-            try {
-                objectOutputStream = new ObjectOutputStream(os);
-                objectOutputStream.writeObject(playerDB);
-                objectOutputStream.flush();
+            Client.clientsVector.entrySet().stream()
+                    .filter(map -> (map.getKey()).equals(login.getUserName()))
+                    .forEach(map -> {
 
-            } catch (IOException ex) {
-                ex.printStackTrace();
+                        try {
+                            objectOutputStream = new ObjectOutputStream(os);
+                            objectOutputStream.writeObject(playerDB);
+                            objectOutputStream.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    });
+            if (!status) {
+                clientsVector.remove(login.getUserName());
             }
         } catch (SQLException ex) {
-            ex.printStackTrace();
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }
 
     void sendRegisterMessage(Register register) {
-        Player p = new Player(register.getUserName(), register.getPassword());
+        boolean status = false;
+        playerDB = null;
         try {
-            // check if it exist in DB or not
-            dataAccessLayer.registerPlayer(p);
 
+            Player p = new Player(register.getUserName(), register.getPassword());
+            if (!dataAccessLayer.checkPlayerForRegister(p)) {
+                playerDB = p;
+                status = true;
+                try {
+                    dataAccessLayer.registerPlayer(p);
+                } catch (SQLException ex) {
+                    Logger.getLogger(Client.class
+                            .getName()).log(Level.SEVERE, null, ex);
+                    //must return messgae there is an error or not by serlization
+                }
+            } else {
+                status = false;
+                playerDB = null;
+            }
+
+            Client.clientsVector.entrySet().stream()
+                    .filter(map -> (map.getKey()).equals(p.getUserName()))
+                    .forEach(map -> {
+
+                        try {
+                            objectOutputStream = new ObjectOutputStream(os);
+                            objectOutputStream.writeObject(playerDB);
+                            objectOutputStream.flush();
+                        } catch (IOException ex) {
+                            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    });
+            if (!status) {
+                clientsVector.remove(p.getUserName());
+            }
         } catch (SQLException ex) {
-            Logger.getLogger(Client.class
-                    .getName()).log(Level.SEVERE, null, ex);
-            //must return messgae there is an error or not by serlization
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
@@ -145,10 +179,9 @@ Client.clientsVector.entrySet().stream()
         try {
             objectOutputStream = new ObjectOutputStream(os);
             objectOutputStream.writeObject(connection);
-
-            System.out.println("pass");
         } catch (IOException ex) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Client.class
+                    .getName()).log(Level.SEVERE, null, ex);
         }
 
     }
