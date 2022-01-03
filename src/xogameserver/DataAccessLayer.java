a7b65574748681164758e1f8b3c822a0cff5f3ef
raiaan <raiaanm@gmail.com>
2022-01-03 08:57:21 -0800

    handle database exception

--- src/xogameserver/DataAccessLayer.java
+++ src/xogameserver/DataAccessLayer.java
@@ -46,10 +46,9 @@ public class DataAccessLayer {
             connection = DriverManager.getConnection("jdbc:derby://localhost:1527/GameDB", "root", "root");
 
         } catch (SQLNonTransientConnectionException ex) {
-            // show some pop
-            System.out.println("ss");
+               CustomPopup.databaseError();
         } catch (SQLException ex) {
-            Logger.getLogger(DataAccessLayer.class.getName()).log(Level.SEVERE, null, ex);
+            CustomPopup.databaseError();
         }
     }
 
